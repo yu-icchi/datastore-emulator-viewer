e2ce77c128afbd2bacf5921d650fd25dbb4cc734
y_icchi <yu.ichiko@gmail.com>
2018-04-05 10:11:04 +0900

    Update datastore.js

--- test/server/service/datastore.js
+++ test/server/service/datastore.js
@@ -4,31 +4,31 @@ const datastore = require('../../../server/service/datastore')
 
 describe('server/service/datastore', () => {
   it('OK_Kinds', async () => {
-    const kinds = await datastore.getKinds('vigo-dev')
+    const kinds = await datastore.getKinds('')
     console.log(kinds)
   })
 
   it('OK_Property', async () => {
-    const properties = await datastore.getProperty('vigo-dev', '_Task')
+    const properties = await datastore.getProperty('', '_Task')
     console.log(properties)
   })
 
   it('OK_load', async () => {
-    const ret = await datastore.load('vigo-dev', ['Task2', 'task02'])
+    const ret = await datastore.load('', ['Task2', 'task02'])
     console.log(JSON.stringify(ret, null, 2))
   })
 
   it('OK_save', async () => {
-    const ret = await datastore.save('vigo-dev', ['_Test2', 'sample01'], {id: 'sample02', num: 3.15, bool: true})
+    const ret = await datastore.save('', ['_Test2', 'sample01'], {id: 'sample02', num: 3.15, bool: true})
     console.log(JSON.stringify(ret, null, 2))
   })
 
   it('OK_find', async () => {
-    const ret = await datastore.find('vigo-dev', 'Task')
+    const ret = await datastore.find('', 'Task')
     console.log(JSON.stringify(ret, null, 2))
   })
 
   it('OK_total', async () => {
-    await datastore.total('vigo-dev')
+    await datastore.total('')
   })
 })
